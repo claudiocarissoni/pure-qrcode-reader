70be06a1a5e153c85e1fa970170dbf32b67f82fb
claudiocarissoni <claudio.carissoni@gmail.com>
2024-08-16 15:19:22 +0200

    Eliminata dipendenza

--- src/qrreader3.tsx
+++ src/qrreader3.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useZxing } from "react-zxing";
-import { Navigate, useNavigate } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const BarcodeScanner = () => {
   const navigate = useNavigate();
